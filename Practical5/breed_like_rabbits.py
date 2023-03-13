52673507b2317c6b1301febe012e4b46d247c81c
PPXBB <125983217+PPXBB@users.noreply.github.com>
2023-03-13 19:09:35 +0800

    Update breed_like_rabbits.py

--- Practical5/breed_like_rabbits.py
+++ Practical5/breed_like_rabbits.py
@@ -6,9 +6,9 @@
 # Print a message stating the generation at which the total number of rabbits exceeds 100
 
 # Initialize the number of rabbits to 2
-generation = 1  
+rabbits_number = 2
 # Initialize the generation counter to 1
-rabbits_number = 2 
+generation = 1  
 # Use a while loop to simulate rabbit breeding until the total number of rabbits exceeds 100
 while rabbits_number <= 100:
 # In each iteration of the loop, double the number of rabbits
