5a4617dfa403a6381de3d8ffd20f703214c6e88b
PPXBB <qiuxu.22@intl.zju.edu.cn>
2023-03-13 09:05:13 +0100

    create variables.py

---
+++ Practical5/variables.py
@@ -0,0 +1,18 @@
+#store the longitude of different places
+a = -3.19
+b = -118.24
+c = 116.39
+#calculate the difference and store them
+d = abs(a-b)
+e = abs(a-c)
+#compare the difference, and print the answer
+print (d > e)
+#the answer is FALSE, so e is greater.
+#Rob travel further to Haining.
+
+X = True
+Y = False
+W = X and Y
+Z = X or Y
+print (W,Z)
+#W == False, Z == True
