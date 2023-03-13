5115c83390b643ea09f6aec2e17b369847644516
PPXBB <qiuxu.22@intl.zju.edu.cn>
2023-03-13 12:02:57 +0100

    edit hexagons.py

--- Practical5/hexagons.py
+++ Practical5/hexagons.py
@@ -1,3 +1,8 @@
+# Plan:
+# Loop through the integers 1 to 5
+# For each integer i, calculate its hexagonal number using the formula h = i(2i-1)
+# Print the hexagonal number
+
 # Loop through the first five integers and calculate their hexagonal number
 for i in range (1,6):
 	h = i*(2*i-1)
